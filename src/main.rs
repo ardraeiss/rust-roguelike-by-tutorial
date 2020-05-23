5d5fdd8ba1a88a6c6e7b7473c836d963c6f15272
Mikhail Merzlyutin <merzlyutinme@gmail.com>
2020-05-23 04:07:40 +0300

    Created test entities

--- src/main.rs
+++ src/main.rs
@@ -33,10 +33,37 @@ fn main() -> rltk::BError {
     let context = RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
         .build()?;
+
+    // Game state and ECS components
     let mut gs = State {
         ecs: World::new()
     };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+
+    // Entities
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
+        
+    for i in 0..10 {
+        gs.ecs
+        .create_entity()
+        .with(Position { x: i * 7, y: 20 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('☺'),
+            fg: RGB::named(rltk::RED),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
+    }
+
+    // Run the main game loop
     rltk::main_loop(context, gs)
 }
