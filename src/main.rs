d572655a5d400ade5099f06717b1c2158c4aeae7
Mikhail Merzlyutin <merzlyutinme@gmail.com>
2020-05-26 03:16:38 +0300

    Added movement to entities

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,27 @@ use specs_derive::Component;
 struct State {
     ecs: World,
 }
+
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        
+
+        self.run_systems();
+
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
+    }
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker{};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
     }
 }
 
@@ -33,6 +44,25 @@ struct Renderable {
     bg: RGB,
 }
 
+// Kind of "Flag Component" to filter entities that should move
+#[derive(Component)]
+struct LeftMover {}
+
+// The actual entities mover!
+struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>,
+                        WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
+        for (_lefty,pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 { pos.x = 79; }
+        }
+    }
+}
+
 // Application entry point
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
@@ -46,6 +76,7 @@ fn main() -> rltk::BError {
     };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
 
     // Entities
     gs.ecs
@@ -57,7 +88,7 @@ fn main() -> rltk::BError {
             bg: RGB::named(rltk::BLACK),
         })
         .build();
-        
+
     for i in 0..10 {
         gs.ecs
         .create_entity()
@@ -67,6 +98,7 @@ fn main() -> rltk::BError {
             fg: RGB::named(rltk::RED),
             bg: RGB::named(rltk::BLACK),
         })
+        .with(LeftMover{})
         .build();
     }
 
