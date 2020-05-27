f7e9da7d4286b01fd491bb9b7d679f077c9793fc
Mikhail Merzlyutin <merzlyutinme@gmail.com>
2020-05-27 04:53:55 +0300

    Extracted player module

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
+use rltk::{GameState, Rltk, RGB};
 use specs::prelude::*;
 
 pub mod components;
 use components::*;
-mod map;
+pub mod player;
+use player::*;
+pub mod map;
+use map::*;
 
 // Game state
-struct State {
+pub struct State {
     ecs: World,
 }
 
@@ -32,34 +35,6 @@ impl GameState for State {
 impl State {
     fn run_systems(&mut self) {
         self.ecs.maintain();
-    }
-}
-
-fn player_input(gs: &mut State, ctx: &mut Rltk) {
-    // Player movement
-    match ctx.key {
-        None => {} // Nothing happened
-        Some(key) => match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
-            _ => {}
-        },
-    }
-}
-
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<map::TileType>>();
-
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = map::xy_idx(pos.x + delta_x, pos.y + delta_y);
-        if map[destination_idx] != map::TileType::Wall {
-            pos.x = min(79, max(0, pos.x + delta_x));
-            pos.y = min(49, max(0, pos.y + delta_y));
-        }
     }
 }
 
