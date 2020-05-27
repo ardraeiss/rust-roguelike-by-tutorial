ed1672e6d3e65cf4fecb818d14c95166847b6c3e
Mikhail Merzlyutin <merzlyutinme@gmail.com>
2020-05-27 07:36:52 +0300

    Added corridors creation

--- src/map.rs
+++ src/map.rs
@@ -1,5 +1,6 @@
 use rltk::{Rltk, RGB};
 use super::{Rect};
+use std::cmp::{min, max};
 
 #[derive(PartialEq, Copy, Clone)]
 pub enum TileType {
@@ -42,6 +43,7 @@ pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
 
     apply_room_to_map(&room1, &mut map);
     apply_room_to_map(&room2, &mut map);
+    apply_horizontal_tunnel(&mut map, 25, 40, 23);
 
     map
 }
@@ -55,6 +57,24 @@ fn create_boundaries_walls(map: &mut [TileType]) {
     for y in 0..50 {
         map[xy_idx(0, y)] = TileType::Wall;
         map[xy_idx(79, y)] = TileType::Wall;
+    }
+}
+
+fn apply_horizontal_tunnel(map: &mut [TileType], x1:i32, x2:i32, y:i32) {
+    for x in min(x1,x2) ..= max(x1,x2) {
+        let idx = xy_idx(x, y);
+        if idx > 0 && idx < 80*50 {
+            map[idx as usize] = TileType::Floor;
+        }
+    }
+}
+
+fn apply_vertical_tunnel(map: &mut [TileType], y1:i32, y2:i32, x:i32) {
+    for y in min(y1,y2) ..= max(y1,y2) {
+        let idx = xy_idx(x, y);
+        if idx > 0 && idx < 80*50 {
+            map[idx as usize] = TileType::Floor;
+        }
     }
 }
 
